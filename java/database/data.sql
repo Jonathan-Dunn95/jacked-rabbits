55c5f8e91be532c0b1dbcee32a3c1e0a6a7d8a48
Jason Kester <jkbirdstar@gmail.com>
2023-08-08 14:09:44 -0400

    added children to data script

--- java/database/data.sql
+++ java/database/data.sql
@@ -2,5 +2,10 @@ BEGIN TRANSACTION;
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+INSERT INTO users (username,password_hash,role) VALUES ('jason','$2a$10$tZSngHOSzO6CxBhz4O1Vue7HidItv2eCz902cYoKYfbzfxa2p3oGm','ROLE_PARENT'); -- password is password
+
+INSERT INTO kids (user_id,username,password_hash,carrots) VALUES (3,'timmy','$2a$10$tZSngHOSzO6CxBhz4O1Vue7HidItv2eCz902cYoKYfbzfxa2p3oGm',4);
+INSERT INTO kids (user_id,username,password_hash,carrots) VALUES (3,'jimmy','$2a$10$tZSngHOSzO6CxBhz4O1Vue7HidItv2eCz902cYoKYfbzfxa2p3oGm',23);
+INSERT INTO kids (user_id,username,password_hash,carrots) VALUES (3,'tommy','$2a$10$tZSngHOSzO6CxBhz4O1Vue7HidItv2eCz902cYoKYfbzfxa2p3oGm',135);
 
 COMMIT TRANSACTION;
