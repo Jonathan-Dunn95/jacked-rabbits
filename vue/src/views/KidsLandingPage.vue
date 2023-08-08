83654624188a1c2ef835c7ff64f5561d0ed13376
Jason Kester <jkbirdstar@gmail.com>
2023-08-08 13:54:06 -0400

    actually fixed the code

--- vue/src/views/KidsLandingPage.vue
+++ vue/src/views/KidsLandingPage.vue
@@ -1,13 +1,6 @@
 <template>
   <div>
       <h1>Kids Landing Page</h1>
-      <button v-on:click="addKid">add kids</button>
-      <ul>
-          <!-- Loop through kids in DB -->
-          <li>
-              <h2>child_name</h2>
-          </li>
-      </ul>
   </div>
 </template>
 
