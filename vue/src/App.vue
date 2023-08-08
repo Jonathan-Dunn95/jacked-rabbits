42f1682c391acc1f8901c2165bc75f11e524bdff
Jonathan Dunn <jonathandunn95@outlook.com>
2023-08-07 21:07:01 -0400

    Fixed Login/Logout button switching in the logoHeader

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -19,6 +19,7 @@ export default {
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&display=swap');
+
 body {
   margin: 0;
 }
--- vue/src/components/LogoHeader.vue
+++ vue/src/components/LogoHeader.vue
@@ -7,6 +7,7 @@
         <router-link v-bind:to="{ name: 'home' }">Home</router-link>
         &nbsp;|&nbsp;
         <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+        <router-link v-bind:to="{ name: 'login' }" v-if="$store.state.token === ''">Login</router-link>
         </div>
     </div>
 </template>
