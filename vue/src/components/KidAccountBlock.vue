23902068a8a9ccaebc15394997d8f9f291a90405
Jason Kester <jkbirdstar@gmail.com>
2023-08-08 18:05:08 -0400

    added list of kids and a form to log hours/steps

--- vue/src/components/KidAccountBlock.vue
+++ vue/src/components/KidAccountBlock.vue
@@ -2,16 +2,29 @@
   <div>
       <div class="kid-block">
         <button v-on:click="addKid">add kids</button>
-        <!-- <ul>
+        <ul>
           <li v-for="kid in kids" v-bind:key="kid.id">
               <h2>{{ kid.name }}</h2>
-              <p>Steps: {{ kid.steps }}</p>
-              <p>Hours of Activity: {{ kid.hours }}</p>
-              <button>Log Hours/Steps</button>
-              <button>View Account</button>
-              <button>Remove Kid</button>
+              <div class="kid-details">
+                <p>Steps: {{ kid.steps }}</p>
+                <p>Hours of Activity: {{ kid.hours }}</p>
+                <p>Carrots: {{ kid.carrots }}</p>
+              </div>
+              <div class="buttons">
+                <button v-on:click="showForm(kid.id)">Log Hours/Steps</button>
+                <button>View Account</button>
+                <button>Remove Kid</button>
+              </div>
           </li>
-        </ul> -->
+        </ul>
+        <form v-on:submit.prevent="updateKidActivity" class="log-hours-form"  v-if="isFormShown">
+          <label for="steps">steps: </label>
+          <input type="number" id="steps" v-model="activityForm.steps">
+          <label for="hours">hours: </label>
+          <input type="number" id="hours" v-model="activityForm.hours">
+          <button v-on:click="hideForm"></button>
+          <input type="submit">
+        </form>
       </div>
   </div>
 </template>
@@ -25,7 +38,35 @@ export default {
       newKid: {
         name: "jean",
         steps: 1
-      }
+      },
+      activityForm: {
+        steps: 0,
+        hours: 0
+      },
+      currentKidId: 0,
+      kids: [
+        {
+          id: 1,
+          name: "timmy",
+          steps: 0,
+          hours: 0,
+          carrots: 4
+        },
+        {
+          id: 2,
+          name: "jimmy",
+          steps: 0,
+          hours: 0,
+          carrots: 23
+        },
+        {
+          id: 3,
+          name: "tommy",
+          steps: 0,
+          hours: 0,
+          carrots: 135
+        }
+      ]
     }
   },
   methods: {
@@ -34,6 +75,22 @@ export default {
       KidService.createKid(this.$store.state.user.id, this.newKid).then(
 
       )
+    },
+    showForm(kidId) {
+      this.currentKidId = kidId;
+    },
+    hideForm() {
+      this.currentKidId = 0;
+    },
+    updateKidActivity() {
+      this.kids[this.currentKidId-1].steps += parseInt(this.activityForm.steps);
+      this.kids[this.currentKidId-1].hours += parseInt(this.activityForm.hours);
+      this.hideForm();
+    }
+  },
+  computed: {
+    isFormShown() {
+      return this.currentKidId > 0;
     }
   }
 }
@@ -52,5 +109,18 @@ export default {
   }
   button:active {
     background-color: #26f1b4;
+  }
+  li {
+    display: inline-block;
+    margin: 40px
+  }
+  .kid-details {
+    display: flex;
+  }
+  .kid-details > p {
+    margin-right: 20px;
+  }
+  .buttons > button {
+    margin-right: 20px;
   }
 </style>
