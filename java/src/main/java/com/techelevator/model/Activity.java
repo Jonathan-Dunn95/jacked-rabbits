14783cdf24982f395a25a4d42921c33dd7d755bb
ThanhPham <phuongthanhpham0501@gmail.com>
2023-08-10 09:17:05 -0400

    activity.java

--- java/src/main/java/com/techelevator/model/Activity.java
+++ java/src/main/java/com/techelevator/model/Activity.java
@@ -18,6 +18,10 @@ public class Activity {
     public int getKidId() {
         return kidId;
     }
+    //implement constructor without parameters
+    public Activity(){
+
+    }
 
     public void setKidId(int kidId) {
         this.kidId = kidId;
