b7d749af18981d872c5da077e65513b0ba449070
mills <treizekhushrenada88@gmail.com>
2023-08-11 23:35:50 -0400

    controller update

--- java/src/main/java/com/techelevator/controller/ClosetController.java
+++ java/src/main/java/com/techelevator/controller/ClosetController.java
@@ -1,6 +1,5 @@
 package com.techelevator.controller;
 
-
 import com.techelevator.dao.ClosetDao;
 import com.techelevator.model.Closet;
 import org.springframework.web.bind.annotation.*;
@@ -8,35 +7,34 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/closet")
 @CrossOrigin
 public class ClosetController {
+
     private ClosetDao closetDao;
 
     public ClosetController(ClosetDao closetDao) {
         this.closetDao = closetDao;
     }
 
-    @RequestMapping(path = "/closet", method = RequestMethod.GET)
-    public List<Closet> getAllClosetItems(){
+    @GetMapping
+    public List<Closet> getAllClosetItems() {
         return closetDao.getAllClosetItems();
     }
 
-
-    @RequestMapping(path = "/closet/{kidId}", method = RequestMethod.GET)
-    public List<Closet> getItemsByKidId(@PathVariable int kidId) {
-        return closetDao.getItemsByKidId(kidId);
+    @GetMapping("/{closetId}")
+    public Closet getClosetItemById(@PathVariable int closetId) {
+        return closetDao.getClosetItemById(closetId);
     }
 
-
-    @RequestMapping(path = "/closet/add", method = RequestMethod.POST)
-    public void addItemToCloset(@RequestParam int item_id, int mascot_id){
-        this.closetDao.addItemToCloset(item_id, mascot_id);
+    @PostMapping("/add")
+    public void addItemToCloset(@RequestParam int item_id, @RequestParam int mascot_id) {
+        closetDao.addItemToCloset(item_id, mascot_id);
     }
 
-    @RequestMapping(path = "/closet/{item_id}")
-    public void deleteItem(@PathVariable int item_id) {
-        closetDao.deleteItem(item_id);
+    @PutMapping("/{closetId}")
+    public void updateClosetItem(@PathVariable int closetId, @RequestBody Closet closetItem) {
+        closetItem.setCloset_id(closetId);
+        closetDao.updateClosetItem(closetItem);
     }
-
-
 }
