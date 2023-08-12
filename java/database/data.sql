79a943c980e2286681256bb7a6481ed14aa25f5d
Jason Kester <jkbirdstar@gmail.com>
2023-08-12 11:35:20 -0400

    Made users when creating a kid account

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,14 +3,17 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 INSERT INTO users (username,password_hash,role) VALUES ('jason','$2a$10$tZSngHOSzO6CxBhz4O1Vue7HidItv2eCz902cYoKYfbzfxa2p3oGm','ROLE_PARENT'); -- password is password
+INSERT INTO users (username,password_hash,role) VALUES ('timmy','$2a$10$tZSngHOSzO6CxBhz4O1Vue7HidItv2eCz902cYoKYfbzfxa2p3oGm','ROLE_KID'); -- password is password
+INSERT INTO users (username,password_hash,role) VALUES ('jimmy','$2a$10$tZSngHOSzO6CxBhz4O1Vue7HidItv2eCz902cYoKYfbzfxa2p3oGm','ROLE_KID'); -- password is password
+INSERT INTO users (username,password_hash,role) VALUES ('tommy','$2a$10$tZSngHOSzO6CxBhz4O1Vue7HidItv2eCz902cYoKYfbzfxa2p3oGm','ROLE_KID'); -- password is password
 
-INSERT INTO kids (user_id,username,password_hash,carrots) VALUES (3,'timmy','$2a$10$tZSngHOSzO6CxBhz4O1Vue7HidItv2eCz902cYoKYfbzfxa2p3oGm',4);
-INSERT INTO kids (user_id,username,password_hash,carrots) VALUES (3,'jimmy','$2a$10$tZSngHOSzO6CxBhz4O1Vue7HidItv2eCz902cYoKYfbzfxa2p3oGm',23);
-INSERT INTO kids (user_id,username,password_hash,carrots) VALUES (3,'tommy','$2a$10$tZSngHOSzO6CxBhz4O1Vue7HidItv2eCz902cYoKYfbzfxa2p3oGm',135);
+INSERT INTO kids (kids_id, user_id,username,password_hash,carrots) VALUES (4,3,'timmy','$2a$10$tZSngHOSzO6CxBhz4O1Vue7HidItv2eCz902cYoKYfbzfxa2p3oGm',4);
+INSERT INTO kids (kids_id, user_id,username,password_hash,carrots) VALUES (5,3,'jimmy','$2a$10$tZSngHOSzO6CxBhz4O1Vue7HidItv2eCz902cYoKYfbzfxa2p3oGm',23);
+INSERT INTO kids (kids_id, user_id,username,password_hash,carrots) VALUES (6,3,'tommy','$2a$10$tZSngHOSzO6CxBhz4O1Vue7HidItv2eCz902cYoKYfbzfxa2p3oGm',135);
 
-INSERT INTO activity (kids_id,steps,minutes) VALUES (1,'40',5);
-INSERT INTO activity (kids_id,steps,minutes) VALUES (2,'100',20);
-INSERT INTO activity (kids_id,steps,minutes) VALUES (3,'230',60);
+INSERT INTO activity (kids_id,steps,minutes) VALUES (4,'40',5);
+INSERT INTO activity (kids_id,steps,minutes) VALUES (5,'100',20);
+INSERT INTO activity (kids_id,steps,minutes) VALUES (6,'230',60);
 
 -- Shirts
 INSERT INTO items_store (item_id,img_url) VALUES (1,'https://mykidswellnessapp-media.s3.amazonaws.com/all_items/shirts/shirt1.jpg');
